0f212849ff9aa9f874d82be04d832b155704f48c
Vlad <vb@cylex.ro>
2013-07-18 16:19:35 +0300

    REmoved breadcrumb

--- sites/all/themes/social/templates/page.tpl.php
+++ sites/all/themes/social/templates/page.tpl.php
@@ -61,7 +61,6 @@
       <?php if (!empty($page['highlighted'])): ?>
         <div class="highlighted hero-unit"><?php print render($page['highlighted']); ?></div>
       <?php endif; ?>
-      <?php if (!empty($breadcrumb)): print $breadcrumb; endif;?>
       <a id="main-content"></a>
       <?php print render($title_prefix); ?>
       <?php if (!empty($title)): ?>
